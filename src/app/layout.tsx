e69253fb88b098bf0c781a03c85fdf24fbaaa113
araviart <alexandre.raviart@etu.univ-orleans.fr>
2024-09-21 10:24:07 +0200

    init config shadcn maxwidhtwrapper fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+const nuckleRegular = localFont({
+  src: "./fonts/Nuckle-Regular.woff",
   variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
   weight: "100 900",
 });
 
@@ -24,11 +20,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          "relative h-full font-sans antialiased",
+          nuckleRegular.className
+        )}
       >
-        {children}
+        <main className="relative flex flex-col min-h-screen">
+          <div className="flex-grow flex-1">{children}</div>
+        </main>
       </body>
     </html>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+const Navbar = () => {
+  return (
+    <div className="bg-white sticky z-50 top-0 inset-x-9 h-16"></div>
+  )
+}
+
+export default Navbar
